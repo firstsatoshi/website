cf678fa309f86dde02f857a294e82a288b6803ce
yqq <youngqqcn@gmail.com>
2023-05-29 19:06:36 +0800

    update coin price api

--- internal/logic/coinPrice/coinpricelogic.go
+++ internal/logic/coinPrice/coinpricelogic.go
@@ -2,6 +2,12 @@ package coinPrice
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/fantopia-dev/website/internal/svc"
 	"github.com/fantopia-dev/website/internal/types"
@@ -23,8 +29,61 @@ func NewCoinPriceLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CoinPri
 	}
 }
 
-func (l *CoinPriceLogic) CoinPrice() (resp *types.CoinPriceResp, err error) {
-	// todo: add your logic here and delete this line
+func (l *CoinPriceLogic) CoinPrice() (*types.CoinPriceResp, error) {
+	key := "bitcion-price-usd"
+	timeout := 3600
 
-	return
+	v, err := l.svcCtx.Redis.Get(key)
+	if err == nil {
+		price, err := strconv.ParseFloat(v, 64)
+		if err == nil {
+			return &types.CoinPriceResp{
+				BtcPriceUsd: price,
+			}, nil
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+
+		type DataItem struct {
+			PriceUsd string `json:"priceUsd"`
+		}
+		type Resp struct {
+			Data DataItem `json:"data"`
+		}
+
+		rsp, err := http.Get("https://api.coincap.io/v2/assets/bitcoin")
+		if err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		if rsp.StatusCode != http.StatusOK {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		body, err := ioutil.ReadAll(rsp.Body)
+		if err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		var r Resp
+		if err = json.Unmarshal(body, &r); err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		l.svcCtx.Redis.Setex(key, r.Data.PriceUsd, timeout)
+
+		price, err := strconv.ParseFloat(v, 64)
+		if err == nil {
+			return &types.CoinPriceResp{
+				BtcPriceUsd: price,
+			}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("coincap api error")
 }
