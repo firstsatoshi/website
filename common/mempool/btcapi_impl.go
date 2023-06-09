3892d5d206823610bd0c699a41891951f7eee0c6
yqq <youngqqcn@gmail.com>
2023-06-09 17:37:47 +0800

    fix broadcast with 60 times

--- common/mempool/btcapi_impl.go
+++ common/mempool/btcapi_impl.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
@@ -70,35 +71,51 @@ func (m *MempoolApiClient) GetRawTransaction(txHash *chainhash.Hash) (*wire.MsgT
 }
 
 // BroadcastTx https://mempool.space/zh/docs/api/rest#post-transaction
-func (m *MempoolApiClient) BroadcastTx(tx *wire.MsgTx) (*chainhash.Hash, error) {
+func (m *MempoolApiClient) BroadcastTx(tx *wire.MsgTx) (txHash *chainhash.Hash, err error) {
 
 	var buf bytes.Buffer
 	if err := tx.Serialize(&buf); err != nil {
+		logx.Errorf("tx.Serialize error: %v", err.Error())
 		return nil, err
 	}
 
 	url := fmt.Sprintf("%s/tx", m.host)
 	// fmt.Printf("url: %v", url)
-	txHex :=  hex.EncodeToString(buf.Bytes())
+	txHex := hex.EncodeToString(buf.Bytes())
 	logx.Infof("BroadcastTx rawtx: %v", txHex)
 
-	resp, err := m.client.R().SetBody([]byte(hex.EncodeToString(buf.Bytes()))).Post(url)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode() != http.StatusOK {
-		err = fmt.Errorf("code:%v,error:%v", resp.StatusCode(), string(resp.Body()))
-		return nil, err
+	txHash = nil
+	txid := ""
+	for iTry := 0; iTry < 60; iTry++ {
+		logx.Infof(" BroadcastTx  try %v times ", iTry)
+		time.Sleep(time.Second * 1)
+		resp, err := m.client.R().SetBody([]byte(hex.EncodeToString(buf.Bytes()))).Post(url)
+		if err != nil {
+			continue
+		}
+		if resp.StatusCode() != http.StatusOK {
+			continue
+		}
+
+		txid = string(resp.Body())
+		if len(txid) != 64 {
+			continue
+		}
+		_, e := hex.DecodeString(txid)
+		if e != nil {
+			continue
+		}
+
+		// everything is ok
+		txHash, err := chainhash.NewHashFromStr(txid)
+		if err != nil {
+			return nil, errors.Wrap(err, fmt.Sprintf("failed to parse tx hash, %s", string(txid)))
+		}
+		logx.Infof("txid: %v", txHash)
+		return txHash, nil
 	}
 
-	txid := resp.Body()
-	if len(txid) == 0 {
-		return nil, fmt.Errorf("reponse is empty")
-	}
-	txHash, err := chainhash.NewHashFromStr(string(txid))
-	if err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("failed to parse tx hash, %s", string(txid)))
-	}
-	logx.Infof("txid: %v", txHash)
-	return txHash, nil
+	logx.Infof("BroadcastTx rawtx error %v ", err.Error())
+
+	return nil, err
 }
